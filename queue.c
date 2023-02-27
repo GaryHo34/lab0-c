7215b0d90fee0652fb5935ad8b73d230f7bba150
GaryHo34 <garyho0916@gmail.com>
2023-02-26 20:07:09 -0800

    Remove unnecessary statement

--- queue.c
+++ queue.c
@@ -69,10 +69,10 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
     if (head == NULL || list_empty(head))
         return NULL;
     element_t *ele = list_first_entry(head, element_t, list);
-    list_del(&ele->list);
-    if (sp) {
+    if (sp && ele->value) {
         strncpy(sp, ele->value, bufsize - 1);
     }
+    list_del(&ele->list);
     return ele;
 }
 
@@ -82,11 +82,10 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
     if (head == NULL || list_empty(head))
         return NULL;
     element_t *ele = list_last_entry(head, element_t, list);
-    if (sp) {
+    if (sp && ele->value) {
         strncpy(sp, ele->value, bufsize - 1);
     }
     list_del(&ele->list);
-    printf("%s\n", ele->value);
     return ele;
 }
 
